7a7baede80b6e77538b9af1bed1ab5ec28b4fece
Ksenia <ksju.nikitina@rambler.ru>
2020-11-24 16:21:40 +0300

    Геолокация

--- StudentMap/app/src/main/java/com/example/studentmap/MapFragment.java
+++ StudentMap/app/src/main/java/com/example/studentmap/MapFragment.java
@@ -1,5 +1,8 @@
 package com.example.studentmap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +10,29 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
     private MapView mMapView;
+    private GoogleMap map;
     private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+
+    FusedLocationProviderClient client;
+    Location currentLocation;
+
 
     @Nullable
     @Override
@@ -26,13 +43,52 @@ public class MapFragment extends Fragment {
         return rootView;
     }
 
-    private void initGoogleMap(Bundle savedInstanceState){
+    private void initGoogleMap(Bundle savedInstanceState) {
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
         mMapView.onCreate(mapViewBundle);
-        //mMapView.getMapAsync(this);
+        getCurrentLocation();
+    }
+
+    public void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            return;
+        }
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+        Task<Location> task = client.getLastLocation();
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null){
+                        currentLocation = location;
+                        mMapView.getMapAsync(MapFragment.this);
+                    }
+                }
+            });
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        //LatLng latLng = new LatLng(55.751244, 37.618423);
+        MarkerOptions options = new MarkerOptions().position(latLng).title("I am here").visible(true);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+        googleMap.addMarker(options);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 101:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getCurrentLocation();
+                }
+                break;
+        }
     }
 
     @Override
