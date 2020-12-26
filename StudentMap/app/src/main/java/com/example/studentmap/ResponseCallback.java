2ca0005a6bbf15539e5d495fa324e16df09f91f9
olga <voronovaolja2001@mail.ru>
2020-12-26 22:15:49 +0300

    route

--- StudentMap/app/src/main/java/com/example/studentmap/ResponseCallback.java
+++ StudentMap/app/src/main/java/com/example/studentmap/ResponseCallback.java
@@ -1,7 +1,9 @@
 package com.example.studentmap;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface ResponseCallback {
     void response(List<Place> placeList);
+    void responseRoute(List<List<HashMap<String,String>>> route);
 }
